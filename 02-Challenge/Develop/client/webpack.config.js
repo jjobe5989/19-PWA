82c132974427742706f383c967279ea2fe8e8853
jjobe5989 <s3thjobe@gmail.com>
2023-03-07 15:25:58 -0600

    completed todos for webpack.config.js

--- 02-Challenge/Develop/client/webpack.config.js
+++ 02-Challenge/Develop/client/webpack.config.js
@@ -18,12 +18,54 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Webpack Plugin",
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "service-worker.js",
+      }),
+      new WebpackPwaManifest({
+        name: "Just Another Text Editor",
+        short_name: "Jate",
+        description: "Online/Offline text editor",
+        background_color: "#7eb4e2",
+        theme_color: "#7eb4e2",
+        start_url: "/",
+        publicPath: "/",
+        fingerprints: false,
+        inject: true,
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["stlye-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "bable-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins:[
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
